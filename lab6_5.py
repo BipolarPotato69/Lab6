808e324d54085106c26334adb667af2c5376f590
BipolarPotato69 <65852468+BipolarPotato69@users.noreply.github.com>
2021-11-04 19:16:18 +0200

    added get_words() and get_user_words()

--- lab6_5.py
+++ lab6_5.py
@@ -14,11 +14,22 @@ def get_words(f: str, letters: List[str]) -> List[str]:
     """
     Reads the file f. Checks the words with rules and returns a list of words.
     """
-    with open(f, 'r') as file:
-        wordlist = file.read()
+    letters = letters[0] + letters[1] + letters[2]
     
-    return wordlist
+    with open(f, 'r') as file:
+        wordlist = [i.lower() for i in file.read().split() if len(i) >= 4 and letters[4].lower() in i]
+    
+    new_wordlist = []
 
+    for word in  wordlist:
+        for letter in word:
+            if letter.upper() not in letters:
+                break
+            if letters.count(letter.upper()) > word.count(letter.lower()):
+                break
+        else:
+            new_wordlist.append(word)
+    return new_wordlist
 
 
 def get_user_words() -> List[str]:
@@ -26,7 +37,15 @@ def get_user_words() -> List[str]:
     Gets words from user input and returns a list with these words.
     Usage: enter a word or press ctrl+d to finish.
     """
-    pass
+    try:
+        word = input()
+        wordlist = [word]
+        while word != "":
+            user_input = input()
+            wordlist.append(user_input)
+    except EOFError:
+        return wordlist
+    return wordlist
 
 
 def get_pure_user_words(user_words: List[str], letters: List[str], words_from_dict: List[str]) -> List[str]:
